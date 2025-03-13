ca0f3b51dfce009b7cd68b7cbca472e2703bc77a
Shubham Chikane <123558983+shubhamchikane99@users.noreply.github.com>
2025-03-13 22:36:55 +0530

    Update ChatWindow.js

--- src/pages/ChatWindow.js
+++ src/pages/ChatWindow.js
@@ -26,7 +26,7 @@ const ChatWindow = ({ selectedUser, currentUser }) => {
 
     const userId = currentUser.id; // Use user ID directly
     //const socket = new SockJS("http://192.168.245.89:8088/ws");
-    const socket = new SockJS("https://journalapplication-production-da86.up.railway.app/ws");
+    const socket = new SockJS("https://journalapplication-production-29f1.up.railway.app/ws");
 
     const client = new Client({
       webSocketFactory: () => socket,
